1c574a54b04685cec01e3b66b8b4dd0df7384feb
Walid <walidvb@gmail.com>
2013-11-14 13:24:08 +0100

    corrected padding for carousel control

--- sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
+++ sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
@@ -5,37 +5,39 @@
 ?>
 <div class="artists">
 	<?php foreach($rows as $row): ?>
-	<?php 
-	$name = !empty($row['field_link']) ? l($row['field_artist_name']->name, $row['field_link']['url'], array('attributes' => $row['field_link']['attributes'])) : $row['field_artist_name']->name;
-	$info = '(';
-	$i = 0;
-	if(!empty($row['field_label']))
-	{
-		foreach($row['field_label'] as $label)
-		{
-			$info .= '<span class="artist-labels">' .$label->name . '</span>';
-			if(++$i > 0 && $i != count($row['field_label'])) {$info .= ', ';}
-		}
-	}
-	if(!empty($row['field_origin']) && !empty($row['field_label']))
-	{
-		$info .= ' / ';
-	}
+	<div class="artist artist-item">
+		<?php 
+		$name = !empty($row['field_link']) ? l($row['field_artist_name']->name, $row['field_link']['url'], array('attributes' => $row['field_link']['attributes'])) : $row['field_artist_name']->name;
+		$info = '(';
+			$i = 0;
+			if(!empty($row['field_label']))
+			{
+				foreach($row['field_label'] as $label)
+				{
+					$info .= '<span class="artist-labels">' .$label->name . '</span>';
+					if(++$i > 0 && $i != count($row['field_label'])) {$info .= ', ';}
+				}
+			}
+			if(!empty($row['field_origin']) && !empty($row['field_label']))
+			{
+				$info .= ' / ';
+			}
 
-	if(!empty($row['field_origin']))
-	{
-		$info .= '<span class="artist-origin">' . $row['field_origin']->name . '</span>';
-	}
+			if(!empty($row['field_origin']))
+			{
+				$info .= '<span class="artist-origin">' . $row['field_origin']->name . '</span>';
+			}
 
-	$info .= ')';
-	?>
-	<div class="artist">
-		<span class="artist-name">
-			<?php print $name;?>
-		</span>
-		<span class="artist-info">
-			<?php print $info ?>
-		</span>
-	</div>
-<?php endforeach; ?>
+			$info .= ')';
+			?>
+			<div class="artist">
+				<span class="artist-name">
+					<?php print $name;?>
+				</span>
+				<span class="artist-info">
+					<?php print $info ?>
+				</span>
+			</div>
+		</div>
+	<?php endforeach; ?>
 </div>
