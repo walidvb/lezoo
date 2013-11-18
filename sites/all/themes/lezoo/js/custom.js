cef776f486b0bfb6a36bca546986226906d5cf16
Walid <walidvb@gmail.com>
2013-11-18 11:40:39 +0100

    corrects menu for the visual sublinks

--- sites/all/themes/lezoo/js/custom.js
+++ sites/all/themes/lezoo/js/custom.js
@@ -3,7 +3,7 @@
 	Drupal.behaviors.lezoo.attach = function(context) {
 
 		//-------------- stick months above list
-		$('.view-display-id-panel_pane_1 .view-content')
+		$('.page-agenda .view-display-id-panel_pane_1 .view-content')
 		.css('position', 'relative')
 		.stickyHeaders({
 			headlineSelector: 'h3:not(.node-title)',
@@ -15,7 +15,13 @@
 				width: $('.view-id-teaser_list .view-content').width(),
 			});
 		};
-		
+		var activeText = $('.primary .last .dropdown-menu .active-trail a').text();
+		var normalText = $('.primary .last .dropdown-toggle').text();
+		console.log(activeText);
+		if(activeText != '')
+		{
+			$('.primary .last .dropdown-toggle').html(activeText + '<span class="caret"></span>');
+		}
 		//---------------Pin left cols
 		$(".pinned").wrapInner('<div class="pinned-content"/>');
 
@@ -28,7 +34,7 @@
 			$(container).css({
 				display: 'inline-block',
 				width: '100%'
-				});
+			});
 			$('.content').scroll(function(e){
 				console.log(e);
 				$(this).find('.pinnded-content').trigger('scroll', e);
@@ -43,5 +49,7 @@
 		resize();
 		$(window).resize(resize);
 
+		//--------------UUUUUGLY hack
+		$('.bootstrap-twocol-stacked .panel-panel.right').addClass('col-md-6 col-sm-6 col-xs-12');
 	};
 })(jQuery);
