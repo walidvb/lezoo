2c2a9d753ead4838cec805e2fcb55d2f7c6e7bce
ndrik <hvanboetzelaer@gmail.com>
2013-11-18 16:15:01 +0100

    fixed conflicts hopefuly

--- sites/all/themes/lezoo/js/custom.js
+++ sites/all/themes/lezoo/js/custom.js
@@ -1,7 +1,14 @@
 (function($) {
 	Drupal.behaviors.lezoo = {};
 	Drupal.behaviors.lezoo.attach = function(context) {
-
+		//-------------- change menu item
+		var active_trail = $('.primary > .dropdown').find('ul .active-trail a').text();
+		console.log(active_trail);
+		if(active_trail)
+		{
+			$('.primary .dropdown-toggle').html(active_trail + '<span class="caret"></span>');
+		}
+		
 		//-------------- stick months above list
 		$('.page-agenda .view-display-id-panel_pane_1 .view-content')
 		.css('position', 'relative')
@@ -15,6 +22,7 @@
 				width: $('.view-id-teaser_list .view-content').width(),
 			});
 		};
+<<<<<<< HEAD
 		var activeText = $('.primary .last .dropdown-menu .active-trail a').text();
 		var normalText = $('.primary .last .dropdown-toggle').text();
 		console.log(activeText);
@@ -22,6 +30,11 @@
 		{
 			$('.primary .last .dropdown-toggle').html(activeText + '<span class="caret"></span>');
 		}
+=======
+		
+
+
+>>>>>>> frontend
 		//---------------Pin left cols
 		$(".pinned").wrapInner('<div class="pinned-content"/>');
 
@@ -49,7 +62,13 @@
 		resize();
 		$(window).resize(resize);
 
+<<<<<<< HEAD
 		//--------------UUUUUGLY hack
 		$('.bootstrap-twocol-stacked .panel-panel.right').addClass('col-md-6 col-sm-6 col-xs-12');
+=======
+		//-------UUUUUUGLY
+		$('.panel-panel.right').addClass('col-md-6 col-sm-6 col-xs-12');
+>>>>>>> frontend
 	};
+
 })(jQuery);
