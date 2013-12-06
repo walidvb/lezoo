40f9ae14ba4e34c6749e3c43f0975adc5cb1cb22
Walid <walidvb@gmail.com>
2013-12-06 16:01:48 +0100

    adds additional info field to artist

--- sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
+++ sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
@@ -8,6 +8,7 @@
 	<div class="artist artist-item">
 		<?php 
 		$name = !empty($row['field_link']) ? l($row['field_artist_name']->name, $row['field_link']['url'], array('attributes' => $row['field_link']['attributes'])) : $row['field_artist_name']->name;
+		$bracketContent = !empty($row['field_label']) && !empty($row['field_origin']);
 		$info = '(';
 			$i = 0;
 			if(!empty($row['field_label']))
@@ -33,9 +34,16 @@
 			<span class="artist-name">
 				<?php print $name;?>
 			</span>
-			<span class="artist-info">
-				<?php print $info ?>
-			</span>
+			<?php if(!empty($row['field_artist_details'])): ?>
+				<span class="artist-additional-info">
+					<?php print $row['field_artist_details'] ?>
+				</span>
+			<?php endif; ?>
+			<?php if($bracketContent): ?>
+				<span class="artist-info">
+					<?php print $info ?>
+				</span>
+			<?php endif; ?>
 	</div>
 	<?php endforeach; ?>
 </div>
