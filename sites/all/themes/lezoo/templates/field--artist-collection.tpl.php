3908b57ff15699240c9cee1891b0c8945e592733
Walid <walidvb@gmail.com>
2013-12-10 18:08:18 +0100

    corrects origins/label on field--collections-artist

--- sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
+++ sites/all/themes/lezoo/templates/field--artist-collection.tpl.php
@@ -2,49 +2,52 @@
 /**
  *  Field formatter for the field_achievements field collection.
  */
-//dpm($rows);
 ?>
 <div class="artists">
 	<?php foreach($rows as $row): ?>
+	<?php 
+	$name = (!empty($row['field_link']) && !$teaser) ? l($row['field_artist_name']->name, $row['field_link']['url'], array('attributes' => $row['field_link']['attributes'])) : $row['field_artist_name']->name;
+	$origins = array();
+	foreach($row['field_origin'] as $origin)
+	{
+		$origins[] = $origin->name;
+	}
+	$origins_markup = null;
+	if(!empty($origins))
+	{
+		$origins_markup = '<span class="artist-origin">' . implode(', ', $origins) . '</span>';
+	}
+
+	$labels = array();
+	foreach($row['field_label'] as $label)
+	{
+		$labels[] = $label->name;
+	}
+	$labels_markup = null;
+	if(!empty($labels))
+	{
+		$labels_markup = '<span class="artist-labels">' . implode(', ', $labels) . '</span>';
+	}
+
+	$infos = array($labels_markup, $origins_markup);
+	$infos = array_filter($infos, 'strlen');
+	dpm($infos);
+	$info = implode( ' / ',  $infos);
+	?>
 	<div class="artist artist-item">
-		<?php 
-		$name = (!empty($row['field_link']) && !$teaser) ? l($row['field_artist_name']->name, $row['field_link']['url'], array('attributes' => $row['field_link']['attributes'])) : $row['field_artist_name']->name;
-		$bracketContent = !empty($row['field_label']) && !empty($row['field_origin']);
-		$info = '(';
-			$i = 0;
-			if(!empty($row['field_label']))
-			{
-				foreach($row['field_label'] as $label)
-				{
-					$info .= '<span class="artist-labels">' .$label->name . '</span>';
-					if(++$i > 0 && $i != count($row['field_label'])) {$info .= ', ';}
-				}
-			}
-			if(!empty($row['field_origin']) && !empty($row['field_label']))
-			{
-				$info .= ' / ';
-			}
-
-			if(!empty($row['field_origin']) && isset($row['field_origin']->name) )
-			{
-				$info .= '<span class="artist-origin">' . $row['field_origin']->name . '</span>';
-			}
-
-			$info .= ')';
-			?>
-			<span class="artist-name">
-				<?php print $name;?>
-			</span>
-			<?php if(!empty($row['field_artist_details'])): ?>
-				<span class="artist-additional-info">
-					<?php print $row['field_artist_details'] ?>
-				</span>
-			<?php endif; ?>
-			<?php if($bracketContent): ?>
-				<span class="artist-info">
-					<?php print $info ?>
-				</span>
-			<?php endif; ?>
-	</div>
-	<?php endforeach; ?>
+	<span class="artist-name">
+	<?php print $name;?>
+</span>
+<?php if(!empty($row['field_artist_details'])): ?>
+	<span class="artist-additional-info">
+		<?php print $row['field_artist_details'] ?>
+	</span>
+<?php endif; ?>
+	<?php if(!empty($info)): ?>
+	<span class="artist-info">
+		<?php print '(' . $info . ')'; ?>
+	</span>
+<?php endif; ?>
+</div>
+<?php endforeach; ?>
 </div>
