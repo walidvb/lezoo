59022534b2a092f1fefa001edbbe32e290f655ec
Walid <walidvb@gmail.com>
2014-02-18 19:05:22 +0100

    styles sont passe au zoo

--- sites/all/themes/lezoo/templates/field--artist-photo-collection.tpl.php
+++ sites/all/themes/lezoo/templates/field--artist-photo-collection.tpl.php
@@ -5,9 +5,9 @@
 ?>
  <div class="artist-list">
 	<?php foreach($rows as $row): ?>
-		<?php print l($row['field_artist_name']->name, 'node/' . $row['field_event_ref']->nid, array(
+		<?php print l($row['field_artist_name']->name, 'node/' . $row['field_artist_ref']->nid, array(
 			'attributes' => array(
-				'class' => "artist-list",
+				'class' => "artist-list-item",
 				'data-img' => get_img_url('artist_list', 'field_artist_img', $row['node'])
 				)
 		)); ?>
