44905f61394666f72b82fddf74dd1d9ff9b7c2b0
Walid <walidvb@gmail.com>
2014-02-18 18:26:11 +0100

    adds autosubmit

--- sites/all/themes/lezoo/templates/field--artist-photo-collection.tpl.php
+++ sites/all/themes/lezoo/templates/field--artist-photo-collection.tpl.php
@@ -2,11 +2,10 @@
 /**
  *  Field formatter for the field_artists field collection.
  */
-dpm($rows);
 ?>
- <div class="artists">
+ <div class="artist-list">
 	<?php foreach($rows as $row): ?>
-		<?php print l($row['field_artist_name']->name, 'node' . $row['field_event_ref']->nid, array(
+		<?php print l($row['field_artist_name']->name, 'node/' . $row['field_event_ref']->nid, array(
 			'attributes' => array(
 				'class' => "artist-list",
 				'data-img' => get_img_url('artist_list', 'field_artist_img', $row['node'])
