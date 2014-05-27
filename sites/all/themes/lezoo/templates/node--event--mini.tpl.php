55a108f31890d32d896040cc227a27a5d0764675
Walid <walidvb@gmail.com>
2014-05-27 23:58:30 +0200

    fixes event mini

---
+++ sites/all/themes/lezoo/templates/node--event--mini.tpl.php
@@ -0,0 +1,19 @@
+<?php 
+	$background_image = get_img_url('banner_mobile', 'field_big_image', $node);
+?>
+<div class='node-event <?php print $classes ?>' <?php print $attributes; ?> >
+		<div class='event-node dated-node'>
+			<div class="node-content">
+				<div class='header' style="background-image: url('<?php print $background_image ?>')">
+					<div class="event-titles">
+						<div class='event-title title'> 
+							<?php print render($title_prefix); ?>
+							<?php print l("<h1 $title_attributes>$title</h1>",$node->$nid,  array('html' => true)); ?>
+							<?php print render($title_suffix); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php print render($content['flippy_pager']); ?>
+</div>
