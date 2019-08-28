021f881f7752e6b9b1c9bfce9d24d3d62919d95f
Walid <hello@walidvb.com>
2019-08-28 14:01:59 +0200

    handle push and popstate

--- sites/all/themes/lezoo_2019/js/load-more.js
+++ sites/all/themes/lezoo_2019/js/load-more.js
@@ -2,18 +2,30 @@ console.log('config loading');
 (function ($) {
   Drupal.behaviors.leZooloadMore = {};
   Drupal.behaviors.leZooloadMore.attach = function (context) {
+    window.onpopstate = function(evt){
+      loadFromUrl(document.location, true)
+    }
     $('.center-col a, .flippy a', context).on('click', function(evt){
       evt.preventDefault();
       var url = evt.currentTarget.href
+      loadFromUrl(url)
+    })
+    function loadFromUrl(url, popping){
       var loaderSelector = ['.right-col .pane-content', '.left-col .panel-pane:not(.pane-system-main-menu) .pane-content'];
       $('body').addClass('loading');
       $.ajax({
         url: url,
-        success: function(res){
+        success: function (res) {
           handleDone($(res), loaderSelector[0]);
           handleDone($(res), loaderSelector[1]);
+          console.log(res, $(res).find('title'), $(res).find('title').text())
           var title = $(res).find('title').text()
-          History.pushState({}, title, url);
+          if (popping){
+            History.replaceState({}, title, url);
+          }
+          else{
+            History.pushState({}, title, url);
+          }
         },
       })
 
@@ -37,7 +49,7 @@ console.log('config loading');
             $next.removeClass('arriving');
             void $next.get(0).offsetWidth;
             $next.addClass('arrived')
-            $next.one('animationend', function(){
+            $next.one('animationend', function () {
               $next.removeClass('skewer arrived');
             })
             Drupal.attachBehaviors($next)
@@ -45,6 +57,6 @@ console.log('config loading');
         })
       };
       $(loaderSelector.join(',')).addClass('start');
-    })
+    }
   }
 })(jQuery)
